eaeb9ddd37459605e4d61715c86b84fb1cd89e59
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2024-12-01 19:18:24 +0100

    edit js

--- js/menu-index.js
+++ js/menu-index.js
@@ -2,9 +2,9 @@ const menuBtnOpen = document.querySelector('.js-open-menu');
 const menuBtnClose = document.querySelector('.js-close-menu');
 const menu = document.querySelector('.js-menu');
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+menuBtnOpen.addEventListener('click', toggleModal);
+menuBtnClose.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  modal.classList.toggle('is-open');
+  menu.classList.toggle('is-open');
 }
